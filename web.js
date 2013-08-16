cfded16ed5002941982b26fa7333a6529a462dc5
Richard Baker <richard.baker1@me.com>
2013-08-16 22:17:36 +0000

    added fs.read and fs.write lines of code

--- web.js
+++ web.js
@@ -3,11 +3,20 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 21 and More!');
+   response.send('Hello World 21 and More!');
+});
+
+fs.readFile('index.html',['r'], function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+
+fs.writeFile('message.txt', 'Hello Node',function (err){
+    if (err) throw err;
+    console.log('index.html');
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
