c9c7950ec00b69535de69c0119bacac5e134239a
Richard Baker <richard.baker1@me.com>
2013-08-16 22:25:52 +0000

    commented out the new read file code

--- web.js
+++ web.js
@@ -6,15 +6,15 @@ app.get('/', function(request, response) {
    response.send('Hello World 21 and More!');
 });
 
-fs.readFile('index.html',['r'], function (err, data) {
-    if (err) throw err;
-    console.log(data);
-});
+//fs.readFile('index.html',['r'], function (err, data) {
+  //  if (err) throw err;
+    //console.log(data);
+//});
 
-fs.writeFile('message.txt', 'Hello Node',function (err){
-    if (err) throw err;
-    console.log('index.html');
-});
+//fs.writeFile('message.txt', 'Hello Node',function (err){
+  //  if (err) throw err;
+   // console.log('index.html');
+//});
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
